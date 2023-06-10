b53d69a18584deb9fd0fb4e2c4e8be5c43cebe9f
Usha Sai Chintha <ushasai154@gmail.com>
2023-06-10 18:32:08 +0530

    Initial Commit

---
+++ GithubGraphQLApp/GithubGraphQLAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubGraphQLAppApp.swift
+//  GithubGraphQLApp
+//
+//  Created by Usha Sai Chintha on 10/06/23.
+//
+
+import SwiftUI
+
+@main
+struct GithubGraphQLAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
